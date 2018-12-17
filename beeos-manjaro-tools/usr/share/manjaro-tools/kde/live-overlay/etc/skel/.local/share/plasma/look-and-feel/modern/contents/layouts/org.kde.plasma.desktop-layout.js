4ecf78d13224024a4f333c67ece4def17313120f
Aji Priyo Wibowo <ajipwch@gmail.com>
2018-12-17 22:42:48 +0700

    Desktop Configuration
    Configuration from my desktop like Mac OS , for KDE
    For cursor :
    > capitaine-cursors
    > DMZ-MAC
    For Gnome Aplication GTK :
    > McOS-MJV-Dark-mode-Gnome-3.30-1.1
    
    Font :
    > San Francisco
    
    Icon :
    > Macos-sierra-CT-0.8.1
    
    KDE :
    > dark-arc

---
+++ beeos-manjaro-tools/usr/share/manjaro-tools/kde/live-overlay/etc/skel/.local/share/plasma/look-and-feel/modern/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,156 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "540",
+                    "DialogWidth": "720"
+                },
+                "/General": {
+                    "alignment": "1",
+                    "arrangement": "1",
+                    "positions": "1,9,desktop:/trash.desktop,0,1,desktop:/Home.desktop,0,0",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/DarkestHour/contents/images/1920x1080.jpg"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/Configuration/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        },
+                        "/Configuration/Configuration/Configuration/General": {
+                            "favoritesPortedToKAstats": "true"
+                        },
+                        "/Configuration/Configuration/General": {
+                            "favoritesPortedToKAstats": "true"
+                        },
+                        "/Configuration/Configuration/Shortcuts": {
+                            "global": "Alt+F1"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true"
+                        },
+                        "/Configuration/Shortcuts": {
+                            "global": "Alt+F1"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickerdash"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.appmenu"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration/General": {
+                            "length": "1551"
+                        },
+                        "/Configuration/General": {
+                            "length": "1551"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/Configuration": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "84",
+                    "DialogWidth": "1920"
+                }
+            },
+            "height": 1.8888888888888888,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
